41c0b1ab351d5af9d1d7aed6c6da0fc0a56f5633
jodin22sams <jeromekrwc@gmail.com>
2022-10-14 18:00:22 -0700

    work in progress. everything seems to work except when you put 0 in the pop up window, it creates a square of 1 instead of no squares.

--- javascript.js
+++ javascript.js
@@ -71,7 +71,7 @@ divClassSquare.forEach(square => square.addEventListener('mouseleave', (e) => {
 // this number will create a new grid of squares using that number for each side. if the number is 11, then a grid of 11x11 
 // squares are made. it works. but the color feature doesn't work. the color feature is only on the first square and the rest 
 // of the squares don't have the color changing feature.
- 
+
 // another unusual thing is that when the user enters 0, there is still 1 square made in the grid.
 // and that single square has the color change feature. maybe that first square is not really from the code below but is a
 // left over in memory from the first 256 squares? confused on what nodes are still in memory and which ones are no longer
@@ -81,12 +81,15 @@ divClassSquare.forEach(square => square.addEventListener('mouseleave', (e) => {
 
 // solved the color changing feature after the button click by 
 // re-using the divClassSquare.forEach(square => square.addEventListener('mouseenter', (e) => { ... but instead of using the 
-// var divClassSquare, i used another var called const divClassSquareAfterButton = document.querySelectorAll('.square');
-// seems when we reference a node by a var name, it seems we can't always keep using it again. learned that the first time 
+// var divClassSquare, i used another var called const divClassSquareAfterButton = document.querySelectorAll('.square'); this
+// new var is still holding the node list of all the divs class square. <div class='square'>. before i thought the forEach would
+// still work from the earlier lines of code above but the new squares didn't change colors. 
+
+// seems when we reference a node by a var name, it seems some times we can't use that var again? learned that the first time 
 // when i wanted to append multiple divs and i couldn't so i had to use cloneNode. maybe it's best to use things like
 // container.setAttribute('id', 'new-container') to use that div id again but using a new id to then change the children or 
 // create another var that references the same node you want to reuse and reuse it using it's new var name? need to read more 
-// about reusing nodes and their children. 
+// about reusing nodes and their children. also see if toggle class is better than setAttribute?
 
 const outerButton = document.querySelector('#outer'); // gets the id and puts in a var
 const divButton = document.createElement('div'); // creates a new div and puts in a var
@@ -99,8 +102,20 @@ divButton.appendChild(button); // now the structure is div id outer (parent), th
 // container (next child). since div class button is already the first child of div id outer, you can use appendChild and don't 
 // need insertBefore 
 
-/* the below for the click seems to work ok in terms of making a grid with the correct number of squares on each side. but 
-the changing of the colors from the mouse event no longer works. maybe put the color changing in a function and call it? */
+
+
+/* the below is what happens when you click the button. everything above this line is creating the 16x16 grid of squares. showing 
+the color change effect when the mouse enters/leaves the square. and finally adding a button. some important differences from the 
+earlier part of your code from way up above is that the <div id='container'> changes to <div id='new-container> after the button 
+click. this was needed in order to target the children divs of the container in order to remove them before appending the new divs 
+for the squares based on the user's number. 
+
+also the node list of all the <div class='square'> is referenced with 2 vars. up above, it is referenced with 
+divClassSquare. after the button click, it is referenced with divClassSquareAfterButton even though the node list is the same
+<div class='square'>. you had to reference a new var otherwise the colors didn't change.
+ */
+
+
 button.addEventListener('click', (e) => {
     console.log(e);
     const eachSideNumber = Number(window.prompt('Type a number:', ''));  // need to specify between 2 to 100 bc if only 1, then it 
@@ -112,6 +127,10 @@ button.addEventListener('click', (e) => {
     container.setAttribute('id', 'new-container'); // original container var is <div id='container'> from way up top
     // if id exists, update it to new-container, else create an id with value new-container. 
     // the result will be <div id='new-container'> and it stays that way after each button click
+    // before the button click, the <div id='container'> and all its children which are the original 256 squares are shown.
+    // then after each button click, the <div id='container'> becomes <div id='new-container'> and all its children are based 
+    // on that reference. that's why your loop on removing the previous children to append the new children is using the 
+    // id new-container.
 
     // this part removes all children starting from the first child. it goes top down.
     const wasOriginalGrid = document.querySelector('#new-container');
@@ -141,7 +160,12 @@ button.addEventListener('click', (e) => {
         container.appendChild(divClone);
     }
 
-    // 
+    // the below changes the color of the new squares. it is almost identical to lines 33 to 65 which is the changing of the 
+    // colors of the first 16x16 square grid. the only difference is the var name. instead of reusing the same var name that 
+    // held the node list of <div class='square'>, this one uses divClassSquareAfterButton to hold the same node list. now 
+    // the color change works.
+    // i wonder if you put the color change in a function and just call the function then it doesn't matter on the var name of 
+    // divClassSquareAfterButton vs divClassSquare 
     const divClassSquareAfterButton = document.querySelectorAll('.square');
     console.log(divClassSquareAfterButton); // to see the count of nodes after you click the button and get the user's numbers.
     // see the console.log(divClassSquare) to see how many nodes before the button click. seems to show the correct number of 
@@ -163,7 +187,8 @@ button.addEventListener('click', (e) => {
 
     // the above grid-template-columns is from the css file. since the css can't use variables other than specific numbers in the 
     // repeat(), the below inline style can use variables and will resize the grid based on the user number input. also bc it is
-    // an inline style rule, it overrides the css file
+    // an inline style rule, it overrides the css file which means you can leave the css file alone since that is applied to the 
+    // 16x16 grid and when the grid size changes, then the below grid cols/rows are used
     container.style.gridTemplateColumns = `repeat(${eachSideNumber}, minmax(0, 1fr))`;
     container.style.gridAutoRows = 'minmax(0, 1fr)';
 
